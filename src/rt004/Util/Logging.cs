4aa4fa97ca6297c947b253b3b350da0e04f67587
hgandras <hgandras@gmail.com>
2023-02-22 17:48:10 +0100

    Created support class for Logging

---
+++ src/rt004/Util/Logging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util;
+
+/// <summary>
+/// Class for logging/debugging
+/// </summary>
+internal class Logging
+{
+
+
+}
