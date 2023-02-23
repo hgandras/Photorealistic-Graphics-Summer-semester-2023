1b2c749541fb60fc8dc053d3cd4d75f064a14b93
hgandras <hgandras@gmail.com>
2023-02-23 14:06:16 +0100

    Argument parsing and HDR image generation

--- src/rt004/Program.cs
+++ src/rt004/Program.cs
@@ -1,4 +1,8 @@
-﻿using Util;
+﻿using System.Formats.Asn1;
+using System.Numerics;
+using Util;
+using System.Drawing;
+using System.Globalization;
 //using System.Numerics;
 
 namespace rt004;
@@ -6,6 +10,8 @@ namespace rt004;
 
 internal class Program
 {
+  
+
   static void Main(string[] args)
   {
         // Parameters.
@@ -13,6 +19,9 @@ internal class Program
         int wid = 600;
         int hei = 450;
         string fileName = "demo.pfm";
+
+        Color color1 = Color.FromArgb(255, 255, 255);
+        Color color2 = Color.FromArgb(0, 255, 0);
 
         foreach (string arg in args)
         {
@@ -38,6 +47,34 @@ internal class Program
                             hei = 450;
                         }
                         break;
+
+                    case "color1":
+                        if (!UInt32.TryParse(value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint c1))
+                        {
+                            Logging.InvalidArgument(key);
+                            color1 = Color.FromArgb(255, 255, 255);
+
+                        }
+                        else
+                        {
+                            int[] col1 = ColorTools.hex_to_rgb((int)c1);
+                            color1 = Color.FromArgb(col1[0], col1[1], col1[2]);
+                        }
+                        break;
+                    case "color2":
+                        if (!UInt32.TryParse(value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint c2))
+                        {
+
+                            Logging.InvalidArgument(key);
+                            color2 = Color.FromArgb(0, 255, 0);
+                            break;
+                        }
+                        else
+                        {
+                            int[] col2 = ColorTools.hex_to_rgb((int)c2);
+                            color2 = Color.FromArgb(col2[0], col2[1], col2[2]);
+                        }
+                        break;
                     case "file_name":
                         fileName = value;
                         break;
@@ -45,22 +82,37 @@ internal class Program
             }
             else
             {
-                //Specifiy which parameter was wrong
                 Logging.InvalidArgument();              
             }
         }
-    
+
+        
+
         // HDR image.
         FloatImage fi = new FloatImage(wid, hei, 3);
 
         // TODO: put anything interesting into the image.
         // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
+        float radius = (float)Math.Min(wid,hei)/2f;
+                      
         for (int h = 0; h < hei; h++)
         {
           for (int w = 0; w <wid; w++)
           {
-              fi.PutPixel(w,h,new float[3] { 100.3f,0.3f,0.3f});
-          }
+                double centered_x = w - wid / 2d;
+                double centered_y = h - hei / 2d;
+
+                
+                if (Math.Pow((centered_x), 2) + Math.Pow((centered_y), 2) < Math.Pow(radius, 2))
+                {
+                    Color color_rgb = ColorTools.lerpRGB(color1, color2, 1f/hei*(float)h);
+                    fi.PutPixel(w, h, new float[3] { color_rgb.R, color_rgb.G, color_rgb.B });
+                }
+                else
+                    fi.PutPixel(w, h, new float[3] { 256f, 256f, 256f });
+
+                
+            }
         }
 
         //This part might not work correctly
